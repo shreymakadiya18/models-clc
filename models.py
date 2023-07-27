e7c93ae39ef2355696a0de60e64499c13a47f571
Shrey Makadiya <86375553+shreymakadiya18@users.noreply.github.com>
2023-07-27 21:51:57 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,66 @@
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+# Create your models here.
+class job_list(models.Model):
+    job_id = models.AutoField(primary_key=True)
+    Company_name = models.CharField(max_length=50)
+    Company_title = models.CharField( max_length=50)
+    salary_month = models.CharField(max_length=50)
+    job_post = models.CharField(max_length=50)
+    job_desc = models.CharField( max_length=5000)
+    job = models.CharField( max_length=50)
+    
+    def __str__(self) -> str:
+        return self.Company_name
+    
+
+class job_details(models.Model):
+    job_id = models.AutoField(primary_key=True)
+    job_post = models.CharField(max_length=50)
+    job_vacancy = models.IntegerField(max_length=50)
+    experience = models.CharField(max_length=5000)
+    employee_type = models.CharField(max_length=100)
+    position = models.CharField(max_length=50)
+    salary_month = models.CharField(max_length=100)
+    job_desc = models.CharField(max_length=5000)
+    responsibility = models.CharField(max_length=50)
+    qualification = models.CharField(max_length=100)
+    requirements = models.CharField(max_length=100)
+    location = models.CharField(max_length=50)
+    
+    class Meta():
+        unique_together=('job_post', 'position')
+    
+    def __str__(self):
+        return self.job_post
+
+
+class candidate_details(models.Model):
+    candidate_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    photo = models.ImageField()
+    categories = models.CharField(max_length=50)
+    languages = models.CharField(max_length=500)
+    experince = models.CharField(max_length=5000)
+    qualification = models.CharField(max_length=500)
+    phone = PhoneNumberField(unique=True)
+    about_me = models.CharField(max_length=500)
+    education = models.CharField(max_length=5000)
+    experience_desc = models.CharField(max_length=500)
+    projects = models.CharField(max_length=5000)
+    # filefield or filepathfield
+    cv = models.FileField()
+    
+    def __str__(self) -> str:
+        return self.name
+
+class candidate_list(models.Model):
+    job_id = models.ForeignKey(job_list,on_delete=models.CASCADE)
+    cadidate_id = models.ForeignKey(candidate_details,on_delete=models.CASCADE)
+    time = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return job_list.Company_name
+    
+    
